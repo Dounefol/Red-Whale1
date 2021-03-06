2a53ec29f973e532cd585eb6ddae0311917855a5
Dounefol <72952568+Dounefol@users.noreply.github.com>
2021-03-05 21:09:04 -0800

    Add files via upload

---
+++ otherpeople.js
@@ -0,0 +1,4 @@
+var person1, person2, person3, person4;
+class OtherPeople{
+
+}
---
+++ p5.play-boilerplate-master/sketch.js
@@ -0,0 +1,9 @@
+var FinMendoza, KristinDriscoll;
+
+function setup() {
+}
+
+function draw() {
+  createCanvas(1536, 754);
+  background("#920102");  
+}
